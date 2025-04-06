bdad950dd284137d5c7d3812c3b8460741f4f23f
HwanSeok Choi <123754694+Yaho03@users.noreply.github.com>
2025-04-06 13:31:36 +0900

    Update numerical_gradient.py

--- 밑바닥부터시작하는딥러닝/4.신경망 학습/numerical_gradient.py
+++ 밑바닥부터시작하는딥러닝/4.신경망 학습/numerical_gradient.py
@@ -1,16 +1,31 @@
-def numerical_gradient(f,x):
-  h = 1e-4
-  grad = np.zeros_like(x)
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
+    
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        
+        # f(x+h) 계산
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)
+        
+        # f(x-h) 계산
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) 
+        
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
+        
+    return grad
 
-  for idx in range(x.size):
-    tmp_val = x[idx]
-    x[idx] = tmp_val + h
-    fxh1 = f(x)
 
-    x[idx] = tmp_val - h
-    fxh2 = f(x)
-
-    grad[idx] = (fxh1 - fxh2) / (2*h)
-    x[idx] = tmp_val
-
-  return grad
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+        
+        return grad
